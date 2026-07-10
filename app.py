5b684f548175cfb7b4e940683200cb4991fa2ba4
Ameliazzaman Mona <ameliazmona@gmail.com>
2026-07-10 22:43:33 +0700

    update app.py with new hardcoded session3

--- app.py
+++ app.py
@@ -215,9 +215,9 @@ app = FastAPI(title="AI Movie Recommender API")
 session_service = InMemorySessionService()
 
 # --- UNIFIED GLOBAL APPLICATION CONFIGURATION CONSTANTS ---
-GLOBAL_SESSION_ID = "movie_recommender_web_session2"
-GLOBAL_APP_NAME = "movie_rec_app2"
-GLOBAL_USER_ID = "default_web_user2"
+GLOBAL_SESSION_ID = "movie_recommender_web_session3"
+GLOBAL_APP_NAME = "movie_rec_app3"
+GLOBAL_USER_ID = "default_web_user3"
 
 # Global singleton — Runner is expensive to instantiate
 runner = None
